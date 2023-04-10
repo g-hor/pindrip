78e85367d3705dd6f398a38a58489b98d02ef673
Gary <geehor@gmail.com>
2023-04-10 10:09:06 -0400

    Add some styling to create pin form

--- frontend/src/components/Pins/CreatePinForm.js
+++ frontend/src/components/Pins/CreatePinForm.js
@@ -1,7 +1,11 @@
+import { useSelector, useDispatch } from "react-redux";
+import { getCurrentUser } from "../../store/session";
+import Avatar from '../Profile/Avatar';
 import './CreatePin.css';
 
+
 const CreatePinForm = () => {
-  
+  const currentUser = useSelector(getCurrentUser);
 
   return (
     <div id="create-pin-main-bg">
@@ -33,10 +37,31 @@ const CreatePinForm = () => {
           </div>
 
           <div id="create-pin-right-container">
-            <input
+            <textarea
               type="text"
               id="create-pin-title"
               placeholder='Add your title'
+              />
+
+            <div id="create-pin-user-info">
+              <Avatar avatar={currentUser.avatar} />
+              <div>{currentUser.firstName + ' ' + (currentUser.lastName || '')}</div>
+            </div>
+
+            <textarea
+              id="create-pin-description"
+              placeholder="Tell everyone what your Pin is about"
+              >
+              
+            </textarea>
+
+            <div id="add-alt-text-btn">
+              Add alt text
+            </div>
+
+            <input
+              type="text"
+              placeholder="Add a destination link"
               />
           </div>
 
