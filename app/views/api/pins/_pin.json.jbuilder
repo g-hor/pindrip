88bc37f18ce38bddcd443412e29e9f3127cb5af6
Gary <geehor@gmail.com>
2023-04-11 12:10:50 -0400

    Add styling to pin show page

--- app/views/api/pins/_pin.json.jbuilder
+++ app/views/api/pins/_pin.json.jbuilder
@@ -1,4 +1,8 @@
+creator = User.find_by(id: pin.user_id)
+creator = creator.username
+
 json.set! pin.id do
   json.extract! pin, :id, :title, :description, :alt_text, :website
+  json.set! :creator, creator
   json.set! :photo, url_for(pin.photo) if pin.photo.attached?
 end
--- frontend/src/components/Pins/PinShow.js
+++ frontend/src/components/Pins/PinShow.js
@@ -2,6 +2,7 @@ import { useSelector } from "react-redux";
 import { useParams } from "react-router-dom";
 import './PinShow.css';
 import { Modal } from "../../context/modal";
+import Avatar from "../Profile/Avatar";
 import { useState } from "react";
 
 
@@ -9,14 +10,18 @@ const PinShow = () => {
   const { pinId } = useParams();
   const pin = useSelector(state => state.pins[parseInt(pinId)]);
   const [showModal, setShowModal] = useState(true);
+  const creator = useSelector(state => state.users[pin?.creator]);
+
 
   const hidePin = () => {
     setShowModal(false);
   }
 
+  debugger
+
   return (
     <>
-    {showModal && (
+    {showModal && pin && (
       <Modal customClass={'pin-show-modal'} onClose={hidePin}>
         <div id="pin-show-container">
 
@@ -29,7 +34,46 @@ const PinShow = () => {
           </div>
 
           <div id="pin-info-container">
-            holder
+
+            <div id="pin-show-top-bar">
+              <div id="ellipsis-btn">
+                <i className="fa-solid fa-ellipsis" />
+              </div>
+
+              <div id="show-board-drop-save-btn-holder">
+                <div id="show-pin-board-dropdown-btn">
+                  Board name
+                </div>
+                <div 
+                  id="show-pin-save-btn"
+                  // onClick={}
+                  >
+                  Save
+                </div>
+              </div>
+            </div>
+
+            <div id="pin-show-website">
+              {/* {pin?.website} */}
+              <a href="google.com">google.com</a>
+            </div>
+
+            <div id="pin-show-title">
+              {pin?.title}
+            </div>
+
+            <div id="pin-show-description">
+              {/* {pin?.description} */}
+              this is a description Más de 500 Novedades Diarias✓Autodevoluciones✓Envío gratis a partir de 29€✓Pulsa para ver los detalles de Jersey con patrón de rayas de hombros caídos. Compra tus favoritos y entérate de toda la infromación.Más de 500 Novedades Diarias✓Autodevoluciones✓Envío gratis a partir de 29€✓Pulsa para ver los detalles de Jersey con patrón de rayas de hombros caídos. Compra tus favoritos y entérate de toda la infromación.Más de 500 Novedades Diarias✓Autodevoluciones✓Envío gratis a partir de 29€✓Pulsa para ver los detalles de Jersey con patrón de rayas de hombros caídos. Compra tus favoritos y entérate de toda la infromación.Novedades Diarias✓Autodevoluciones✓Envío gratis a partir de 29€✓Pulsa para ver los detalles de Jersey con patrón de rayas de hombros caídos. Compra tus favoritos y entérate de toda la infromación.Más de 500 Novedades Diarias✓Autodevoluciones✓Envío gratis a partir de 29€✓Pulsa para ver los detalles de Jersey con patrón de rayas de hombros caídos. Compra tus favoritos y entérate de toda la infromación.Más de 500 Novedades Diarias✓Autodevoluciones✓Envío gratis a partir de 29€✓Pulsa para ver los detalles de Jersey con patrón de rayas de hombros caídos. Compra tus favoritos y entérate de toda la infromación
+            </div>
+
+            <div id="pin-show-creator-info">
+              <div id="create-pin-user-info">
+                <Avatar avatar={creator?.avatar} />
+                <div>{creator?.firstName + ' ' + (creator?.lastName || '')}</div>
+              </div>
+            </div>
+
           </div>
 
         </div>
