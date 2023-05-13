221cc3c68afcd004db61527bc9620761e921b4c1
Gary <geehor@gmail.com>
2023-05-13 03:14:43 -0400

    Attempt sticky positioning for saved msg

---
+++ frontend/src/components/Saved/index.js
@@ -0,0 +1,14 @@
+
+import './SavedMsg.css';
+
+const SavedMsg = () => {
+
+
+  return (
+    <div id="saved-msg-container">
+      "Drip saved successfully!
+    </div>
+  )
+};
+
+export default SavedMsg;
