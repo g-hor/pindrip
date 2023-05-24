658cb3d00a34d26fa1871c59192e442a76b795b7
Gary <geehor@gmail.com>
2023-05-24 03:29:08 -0400

    Add more variety to seed data and stylize home pins

--- frontend/src/components/Pins/PinIndexItem.js
+++ frontend/src/components/Pins/PinIndexItem.js
@@ -2,11 +2,18 @@ import { useRef } from "react";
 import { useSelector } from "react-redux";
 import { Link, useParams } from "react-router-dom";
 import Avatar from "../Profile/Avatar";
+import { getInitial } from "../../store/user";
 
 const PinIndexItem = ({ pin }) => {
   const { username } = useParams();
-  const avatar = useSelector(state => state?.users[pin?.creator]?.avatar);
+  const showUser = useSelector(state => state?.users[pin?.creator]);
+  const avatar = showUser?.avatar;
   const pinImg = useRef();
+  let initial;
+
+  if (showUser) {
+    initial = getInitial(showUser)
+  };
 
   return (
     <>
@@ -31,8 +38,14 @@ const PinIndexItem = ({ pin }) => {
           <Link
             to={`/${pin?.creator}`}
             >
-              <Avatar avatar={avatar} />
-              {pin?.creator}
+              {avatar ? (
+                <Avatar avatar={avatar} />
+              ) : (
+                <div id="pin-show-creator-initial">{initial}</div>
+              )}
+              <span>
+                {pin?.creator}
+              </span>
           </Link>
         </div>
       )}
