9df480d35bf8df624f95d7ba18838002be1b77fd
Gary <geehor@gmail.com>
2023-06-16 17:50:20 -0400

    Fix splash page to only render images dripping from top

--- frontend/src/components/Splash/index.js
+++ frontend/src/components/Splash/index.js
@@ -3,15 +3,34 @@ import { splashImages } from './images';
 import { useSelector } from 'react-redux';
 import { getCurrentUser } from '../../store/session';
 import './Splash.css';
-import { useEffect } from 'react';
+import { useEffect, useRef } from 'react';
 
 const SplashPage = () => {
   const navigate = useNavigate();
   const currentUser = useSelector(getCurrentUser);
+  const col1 = useRef();
+  const col2 = useRef();
+  const col3 = useRef();
+  const col4 = useRef();
+  const col5 = useRef();
+  const col6 = useRef();
+  const col7 = useRef();
 
   useEffect(() => {
     if (currentUser) navigate("/home");
   }, [navigate, currentUser]);
+
+  useEffect(() => {
+    setTimeout(() => {
+      col1?.current?.classList.add('drip-col');
+      col2?.current?.classList.add('drip-col');
+      col3?.current?.classList.add('drip-col');
+      col4?.current?.classList.add('drip-col');
+      col5?.current?.classList.add('drip-col');
+      col6?.current?.classList.add('drip-col');
+      col7?.current?.classList.add('drip-col');
+    }, 0)
+  }, [])
 
   return (
     <div id="splash-bg-container">
@@ -26,37 +45,37 @@ const SplashPage = () => {
       </div>
 
       <div id="splash-grid-container">
-        <div className='splash-grid-col col-1'>
+        <div className='splash-grid-col col-1' ref={col1}>
           {splashImages.slice(0, 3).map((img, i) => 
             <img src={img} alt="" key={i} />
             )}
         </div>
-        <div className='splash-grid-col col-2'>
+        <div className='splash-grid-col col-2' ref={col2}>
           {splashImages.slice(3, 6).map((img, i) => 
             <img src={img} alt="" key={i} />
             )}
         </div>
-        <div className='splash-grid-col col-3'>
+        <div className='splash-grid-col col-3' ref={col3}>
           {splashImages.slice(6, 9).map((img, i) => 
             <img src={img} alt="" key={i} />
             )}
         </div>
-        <div className='splash-grid-col col-4'>
+        <div className='splash-grid-col col-4' ref={col4}>
           {splashImages.slice(8, 11).map((img, i) => 
             <img src={img} alt="" key={i} />
             )}
         </div>
-        <div className='splash-grid-col col-5'>
+        <div className='splash-grid-col col-5' ref={col5}>
           {splashImages.slice(12, 15).map((img, i) => 
             <img src={img} alt="" key={i} />
             )}
         </div>
-        <div className='splash-grid-col col-6'>
+        <div className='splash-grid-col col-6' ref={col6}>
           {splashImages.slice(16, 19).map((img, i) => 
             <img src={img} alt="" key={i} />
             )}
         </div>
-        <div className='splash-grid-col col-7'>
+        <div className='splash-grid-col col-7' ref={col7}>
           {splashImages.slice(19, 22).map((img, i) => 
             <img src={img} alt="" key={i} />
             )}
